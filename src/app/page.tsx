ee89b5096892e8a9a7720edf9ef8e59cffe0e928
Ayan Pakhira <120088765+ayan471@users.noreply.github.com>
2026-01-16 15:42:33 +0530

    hide the pricing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <BentoGrid />
       <Process />
       <CaseStudies />
-      <Pricing />
+      {/* <Pricing /> */}
       <Faq />
       <Footer />
     </main>
